ae1cc5254a58ad532ae07be671e15742085df79f
VladRadigin <angry.gamer.comi@gmail.com>
2023-04-17 14:17:48 +0300

    Setting up the administration panel for the Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post) # <- Регистрация модели Post в панели администрирования
+class PostAdmin(admin.ModelAdmin):
+  # изменяем отображения модели:
+  list_display = ['title', 'slug', 'author', 'publish', 'status']
+  list_filter = ['status', 'created', 'publish', 'author']
+  prepopulated_fields = {'slug': ('title',)}
+  raw_id_fields = ['author']
+  date_hierarchy = 'publish'
+  ordering = ['status', 'publish']
